5406662387a74b9be83090ee17698429af088960
Matteo Rosato <85708462+MRRcode979@users.noreply.github.com>
2023-07-08 09:16:34 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use clap::{App, Arg};
 use colored::Colorize;
 
 fn main() {
-    let mut adrr: Vec<u8>;
+    let mut adrr: [u8; 4];
     let matches = cli::build().get_matches();
     if let Some(ref matches) = matches.subcommand_matches("build-scss") {
         if matches.value_of("source") == Some("") {
@@ -44,13 +44,13 @@ fn main() {
 
     if let Some(ref matches) = matches.subcommand_matches("serve") {
     if matches.is_present("host") {
-	adrr = vec![192, 168, 1, 13]; 
+	adrr = [192, 168, 1, 13]; 
 	println!("{:?}", adrr);
-	serve(adrr, matches.value_of("port").expect("Unable to parse argument.").parse().unwrap());
+	serve(&adrr, matches.value_of("port").expect("Unable to parse argument.").parse().unwrap());
     } else {
-	adrr = vec![0, 0, 0, 0];
+	adrr = [127, 0, 0, 1];
 	println!("{:?}", adrr);
-	serve(adrr, matches.value_of("port").expect("Unable to parse argument.").parse().unwrap());
+	serve(&adrr, matches.value_of("port").expect("Unable to parse argument.").parse().unwrap());
     }
 }
 }
